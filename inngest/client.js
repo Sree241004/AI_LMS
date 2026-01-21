15096198c596a92b7a2c0500b9621964ea7a13f3
Sree241004 <2210040061@klh.edu.in>
2026-01-21 15:31:53 +0530

    Fix inngest client file

--- inngest/client.js
+++ inngest/client.js
@@ -1,4 +1,4 @@
-import { Inngest } from "inngest";
+import { inngest } from "../../../inngest/client";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "ai-lms" });
